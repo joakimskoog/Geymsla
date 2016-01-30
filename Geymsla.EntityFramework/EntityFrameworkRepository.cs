fa202ef3d7d9dc1c37f3c5ac27c1e325aa1eb9d2
joakimskoog <joakimskog@mail.nu>
2016-01-30 12:44:48 +0100

    Removed sync methods from ef repository

--- Geymsla.EntityFramework/EntityFrameworkRepository.cs
+++ Geymsla.EntityFramework/EntityFrameworkRepository.cs
@@ -18,22 +18,9 @@ namespace Geymsla.EntityFramework
             DbContext = dbContext;
         }
 
-        public T Get(TId id, params Expression<Func<T,object>>[] includeProperties)
-        {
-            return DbContext.Set<T>().Find((object)id);
-        }
-
         public Task<T> GetAsync(TId id, CancellationToken cancellationToken, params Expression<Func<T, object>>[] includeProperties)
         {
             return DbContext.Set<T>().FindAsync(cancellationToken, (object)id);
-        }
-
-        public IEnumerable<T> Get(Func<IQueryable<T>, IQueryable<T>> queryFilter, params Expression<Func<T, object>>[] includeProperties)
-        {
-            var entities = GetAllAsQueryable();
-            var filteredEntities = queryFilter(entities);
-
-            return filteredEntities.IncludeMultiple(includeProperties);
         }
 
         public async Task<IEnumerable<T>> GetAsync(Func<IQueryable<T>, IQueryable<T>> queryFilter, CancellationToken cancellationToken, params Expression<Func<T, object>>[] includeProperties)
