2b49689d77c6323bf95f60a8ba230275840921e5
Joakim Skoog <glasvegas@hotmail.se>
2016-02-01 21:15:22 +0100

    Fixed broken test

--- Geymsla.Tests/IRepositoryExtensionsTests.cs
+++ Geymsla.Tests/IRepositoryExtensionsTests.cs
@@ -329,8 +329,7 @@ namespace Geymsla.Tests
             var repo = MockRepository.GenerateMock<IRepository<Data, int>>();
             repo.Stub( x => x.GetAsync(Arg<Func<IQueryable<Data>, IQueryable<Data>>>.Is.Anything,
                         Arg<CancellationToken>.Is.Anything, Arg<Expression<Func<Data, object>>[]>.Is.Anything)).Return(Task.FromResult(Enumerable.Empty<Data>()));
-            //repo.Stub(x => x.GetAllAsQueryable()).Return(Enumerable.Empty<Data>().AsQueryable());
-            repo.Stub(x => x.Count()).Return(0);
+            repo.Stub(x => x.GetAllAsQueryable()).Return(Enumerable.Empty<Data>().AsQueryable());
 
             var pagedList = await repo.GetPaginatedListAsync(x => x.OrderBy(y => y.Number), 1, 10);
 
